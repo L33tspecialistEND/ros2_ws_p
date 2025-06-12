810056e4866ee41610310d470717ec933c691ad9
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-06-12 08:57:27 +0100

    Add statement after 'spin()'

--- ros2_executor_ws_p/src/executor_demos/src/minimal_executor_node.cpp
+++ ros2_executor_ws_p/src/executor_demos/src/minimal_executor_node.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[])
     auto node = std::make_shared<rclcpp::Node>("minimal_executor_node");
     RCLCPP_INFO(node->get_logger(), "Minimal Executor Node has started!");
     rclcpp::spin(node);     // Creates the default single-threaded executor
+    RCLCPP_INFO(node->get_logger(), "Hello there, do you have the high ground?")
     rclcpp::shutdown();
 
     return 0;
