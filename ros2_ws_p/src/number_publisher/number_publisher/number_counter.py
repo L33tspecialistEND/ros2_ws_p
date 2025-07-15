32b1277f57c2e9215c1d8f0d9f5b7473486dc497
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-15 14:43:03 +0100

    Add service server using custom ResetCounter interface

--- ros2_ws_p/src/number_publisher/number_publisher/number_counter.py
+++ ros2_ws_p/src/number_publisher/number_publisher/number_counter.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from robot_interfaces.srv import ResetCounter
 
 class NumberCounter(Node):
     def __init__(self):
@@ -12,11 +13,23 @@ class NumberCounter(Node):
             self.callback_number,
             10
         )
+        self.reset_counter_service = self.create_service(
+            ResetCounter,
+            "/reset_counter",
+            self.callback_reset_counter
+        )
         self.get_logger().info("[NumberCounter] Node has started.")
 
     def callback_number(self, msg: Int64):
         self.counter_ += msg.data
-        self.get_logger().info("Counter: " + str(self.counter_))    
+        self.get_logger().info("Counter: " + str(self.counter_))
+    
+    def callback_reset_counter(self, request: ResetCounter.Request, response: ResetCounter.Response):
+        self.counter_ = request.reset_value
+        self.get_logger().info("Reset counter to: " + str(self.counter_))
+        response.success = True
+        response.message = "Success"
+        return response
     
 def main(args = None):
     rclpy.init(args = args)
