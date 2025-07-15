addc99e39c77f64fbc23da1e93292e54c46a1930
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-15 13:49:51 +0100

    Add callback and subscriber to number topic

--- ros2_ws_p/src/number_publisher/number_publisher/number_counter.py
+++ ros2_ws_p/src/number_publisher/number_publisher/number_counter.py
@@ -5,7 +5,19 @@ from example_interfaces.msg import Int64
 class NumberCounter(Node):
     def __init__(self):
         super().__init__("number_counter")
+        self.counter_ = 0
+        self.number_subscriber_ = self.create_subscription(
+            Int64,
+            "/number",
+            self.callback_number,
+            10
+        )
+        self.get_logger().info("[NumberCounter] Node has started.")
 
+    def callback_number(self, msg: Int64):
+        self.counter_ += msg.data
+        self.get_logger().info("Counter: " + str(self.counter_))    
+    
 def main(args = None):
     rclpy.init(args = args)
     node = NumberCounter()
