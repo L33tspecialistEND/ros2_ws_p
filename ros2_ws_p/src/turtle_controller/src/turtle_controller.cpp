a88c3c0074607f24a4eea01878333a4ecb0aaafa
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-16 13:02:54 +0100

    Create service to set turtlesim pen color

--- ros2_ws_p/src/turtle_controller/src/turtle_controller.cpp
+++ ros2_ws_p/src/turtle_controller/src/turtle_controller.cpp
@@ -1,8 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "turtlesim/msg/pose.hpp"
+#include "turtlesim/srv/set_pen.hpp"
+#include "robot_interfaces/srv/reset_counter.hpp"
+#include <functional>
+#include <chrono>
 
+using namespace std::chrono_literals;
 using namespace std::placeholders;
+using SetPen = turtlesim::srv::SetPen;
+
 
 class TurtleController : public rclcpp::Node
 {
@@ -17,31 +24,85 @@ class TurtleController : public rclcpp::Node
                 10,
                 std::bind(&TurtleController::callback_pose, this, _1)
             );
+            turtle_pen_client_ = create_client<SetPen>("/turtle1/set_pen");
+
+            on_right_side_ = true;
 
             RCLCPP_INFO(this->get_logger(), "[TurtleController] Node started.");
+        }
+
+        void set_pen_request(uint8_t r, uint8_t g, uint8_t b, uint8_t width)
+        {
+            while(!turtle_pen_client_->wait_for_service(1s))
+            {
+                if(!rclcpp::ok())
+                {
+                    RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for service. Exiting...");
+                    return;
+                }
+                RCLCPP_INFO(this->get_logger(), "Waiting for service...");
+            }
+            auto request = std::make_shared<SetPen::Request>();
+            request->r = r;
+            request->g = g;
+            request->b = b;
+            request->width = width;
+
+            RCLCPP_INFO(this->get_logger(), "Sending request: r: %d, g: %d, b: %d, width: %d",
+                request->r, request->g, request->b, request->width);
+            turtle_pen_client_->async_send_request(
+                request,
+                std::bind(&TurtleController::callback_set_pen_response, this, _1)
+            );
         }
 
     private:
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr move_turtle_publisher_;
         rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr turtle_pose_subscriber_;
+        rclcpp::Client<SetPen>::SharedPtr turtle_pen_client_;
+        bool on_right_side_;
+
+
+        void callback_set_pen_response(rclcpp::Client<SetPen>::SharedFuture future)
+        {
+            auto response = future.get();
+        }
 
         void callback_pose(const turtlesim::msg::Pose::SharedPtr msg)
         {
             auto move_in_circle = geometry_msgs::msg::Twist();
 
-            if (msg->x < 5.5)
+            if (msg->x > 5.5444)
             {
                 move_in_circle.angular.z = 1.0;
                 move_in_circle.linear.x = 1.0;
                 move_turtle_publisher_->publish(move_in_circle);
                 RCLCPP_INFO(this->get_logger(), "Turtle moving in the left half of the plane...");
             }
-            else
+            else if(msg->x <= 5.5444)
             {
                 move_in_circle.angular.z = 2.0;
                 move_in_circle.linear.x = 2.0;
                 move_turtle_publisher_->publish(move_in_circle);
                 RCLCPP_INFO(this->get_logger(), "Turtle moving in the right half of the plane...");
+            }
+
+            if(msg->x > 5.444)
+                on_right_side_ = true;
+            else
+                on_right_side_ = false;
+
+            if(msg->x > 5.5444 && on_right_side_)
+            {
+                set_pen_request(200, 0, 0, 4);
+                RCLCPP_INFO(this->get_logger(), "Setting pen to Red...");
+                on_right_side_ = false;
+            }
+            else if(msg->x <= 5.5444 && !on_right_side_)
+            {
+                set_pen_request(0, 200, 0, 4);
+                RCLCPP_INFO(this->get_logger(), "Setting pen to Green...");
+                on_right_side_ = true;
             }
         }
 };
@@ -51,7 +112,9 @@ int main(int argc, char** argv)
     rclcpp::init(argc, argv);
     auto node = std::make_shared<TurtleController>();
 
-    rclcpp::spin(node);
+    rclcpp::executors::MultiThreadedExecutor executor;
+    executor.add_node(node);
+    executor.spin();
     rclcpp::shutdown();
 
     return 0;
