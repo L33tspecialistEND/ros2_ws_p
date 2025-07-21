6e869b3c2b950a2f57cf2f0ab23e45c7b7ec445b
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-21 16:58:05 +0100

    Add goal canceling functionality

--- ros2_ws_p/src/action_package/action_package/count_up_to_server.py
+++ ros2_ws_p/src/action_package/action_package/count_up_to_server.py
@@ -3,6 +3,8 @@ import time
 from rclpy.node import Node
 from rclpy.action import ActionServer, GoalResponse
 from rclpy.action.server import ServerGoalHandle
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import ReentrantCallbackGroup
 from robot_interfaces.action import CountUpTo
 
 class CountUpToServer(Node):
@@ -14,8 +16,9 @@ class CountUpToServer(Node):
             CountUpTo,
             "/count_up_to",
             goal_callback = self.goal_callback,
-            execute_callback = self.execute_callback)
-
+            execute_callback = self.execute_callback,
+            callback_group = ReentrantCallbackGroup())
+        
         self.get_logger().info("[CountUpTo] Node has started.")
 
     def goal_callback(self, goal_request: CountUpTo.Goal):
@@ -24,6 +27,12 @@ class CountUpToServer(Node):
             self.get_logger().warn("Rejecting the goal: target number must be positive.")
             return GoalResponse.REJECT
         self.get_logger().info("Accepting the goal")
+        return GoalResponse.ACCEPT
+    
+    def cancel_callback(self, goal_handle: ServerGoalHandle):
+        self.get_logger().info("Received a cancel request")
+        # Here you can implement any logic needed to handle cancellation
+        # For now, we just log it and return ACCEPT
         return GoalResponse.ACCEPT
     
     def execute_callback(self, goal_handle: ServerGoalHandle):
@@ -41,6 +50,12 @@ class CountUpToServer(Node):
             self.get_logger().info(str(counter))
             time.sleep(delay)
         
+        if goal_handle.is_cancel_requested:
+            self.get_logger().info("Canceling goal")
+            goal_handle.canceled()
+            result.reached_number = counter
+            return result
+
         goal_handle.succeed()
         result.reached_number = counter
         return result
@@ -48,7 +63,7 @@ class CountUpToServer(Node):
 def main(args = None):
     rclpy.init(args = args)
     node = CountUpToServer()
-    rclpy.spin(node)
+    rclpy.spin(node, MultiThreadedExecutor())
     rclpy.shutdown()
 
 if __name__ == '__main__':
