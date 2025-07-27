cf4ab615e05105b2af492dd6cb3e3d7ee5f5501e
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-27 16:25:00 +0100

    Add a parameter callback for changing a parameter value

--- ros2_ws_p/src/number_publisher/number_publisher/number_publisher.py
+++ ros2_ws_p/src/number_publisher/number_publisher/number_publisher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
+from rclpy.parameter import Parameter
 
 class NumberPublisher(Node):
     def __init__(self):
@@ -11,12 +12,18 @@ class NumberPublisher(Node):
         self.timer_period = self.get_parameter("publish_period").value
         self.number_publisher_ = self.create_publisher(Int64, "/number", 10)
         self.number_timer_ = self.create_timer(self.timer_period, self.publish_number)
+        self.add_on_set_parameters_callback(self.parameters_callback)
         self.get_logger().info("Number Publisher has been started.")
 
     def publish_number(self):
         msg = Int64()
         msg.data = self.number_
         self.number_publisher_.publish(msg)
+
+    def parameters_callback(self, params: list[Parameter]):
+          for param in params:
+                if param.name == "number":
+                      self.number_ = param.value
 
 def main(args = None):
         rclpy.init(args = args)
