8997defe6f1684878b4d0804f488c1e42d1c0959
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-07-28 10:53:01 +0100

    Add callback for updating parameter value

--- ros2_ws_p/src/number_publisher_cpp/src/number_publisher.cpp
+++ ros2_ws_p/src/number_publisher_cpp/src/number_publisher.cpp
@@ -1,48 +1,79 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include <rcl_interfaces/msg/set_parameters_result.hpp> // Added for SetParametersResult
+#include <functional>
+
+using namespace std::placeholders;
 
 class NumberPublisher : public rclcpp::Node
 {
-    public:
-        NumberPublisher() : Node("number_publisher")
-        {
-            this->declare_parameter("number", 2);
-            this->declare_parameter("publish_period", 1.0);
+public:
+    NumberPublisher() : Node("number_publisher")
+    {
+        this->declare_parameter("number", 2);
+        this->declare_parameter("publish_period", 1.0);
 
-            number_ = this->get_parameter("number").as_int();
-            double timer_period = this->get_parameter("publish_period").as_double();
-            
+        number_ = this->get_parameter("number").as_int();
+        double timer_period = this->get_parameter("publish_period").as_double();
 
-            number_publisher_ = this->create_publisher<example_interfaces::msg::Int64>(
-                "/number", 10);
-            number_timer_ = this->create_wall_timer(
-                std::chrono::duration<double>(timer_period),
-                std::bind(&NumberPublisher::publishNumber, this));
-
-            RCLCPP_INFO(this->get_logger(), "[NumberPublisher] Node has been started.");
+        // Validate timer period
+        if (timer_period <= 0.0) {
+            RCLCPP_ERROR(this->get_logger(), "Invalid publish_period: %f. Using default 1.0s", timer_period);
+            timer_period = 1.0;
         }
 
-    private:
-        rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr number_publisher_;
-        rclcpp::TimerBase::SharedPtr number_timer_;
-        int64_t number_ = 2;
+        number_publisher_ = this->create_publisher<example_interfaces::msg::Int64>("/number", 10);
+        number_timer_ = this->create_wall_timer(
+            std::chrono::duration<double>(timer_period),
+            std::bind(&NumberPublisher::publishNumber, this));
+        param_callback_handle_ = this->add_on_set_parameters_callback(
+            std::bind(&NumberPublisher::parametersCallback, this, _1));
 
-        void publishNumber()
-        {
-            auto msg = example_interfaces::msg::Int64();
-            msg.data = number_;
-            number_publisher_->publish(msg);
-            RCLCPP_INFO(this->get_logger(), "Published: '%ld'", msg.data);
+        RCLCPP_INFO(this->get_logger(), "[NumberPublisher] Node has been started.");
+    }
+
+private:
+    rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr number_publisher_;
+    rclcpp::TimerBase::SharedPtr number_timer_;
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback_handle_;
+    int64_t number_ = 2;
+
+    void publishNumber()
+    {
+        auto msg = example_interfaces::msg::Int64();
+        msg.data = number_;
+        number_publisher_->publish(msg);
+        RCLCPP_INFO(this->get_logger(), "Published: '%ld'", msg.data);
+    }
+
+    rcl_interfaces::msg::SetParametersResult parametersCallback(const std::vector<rclcpp::Parameter> &parameters)
+    {
+        rcl_interfaces::msg::SetParametersResult result;
+        result.successful = true;
+
+        for (const auto &param : parameters) {
+            if (param.get_name() == "number") {
+                if (param.get_type() == rclcpp::ParameterType::PARAMETER_INTEGER) {
+                    number_ = param.as_int();
+                    RCLCPP_INFO(this->get_logger(), "Updated number to: %ld", number_);
+                } else {
+                    result.successful = false;
+                    result.reason = "Parameter 'number' must be an integer";
+                    RCLCPP_ERROR(this->get_logger(), "%s", result.reason.c_str());
+                }
+            }
         }
+        return result;
+    }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<NumberPublisher>();
     rclcpp::spin(node);
-    rclcpp::shutdown();
     RCLCPP_INFO(node->get_logger(), "[NumberPublisher] Node has been shut down.");
+    rclcpp::shutdown();
 
     return 0;
 }
